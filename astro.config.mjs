de6df2ca29abd612651aaefca35454110b51c798
Alex Jimenez <ajimenez@creativoapp.com>
2026-08-06 00:02:56 -0500

    Upgrade Astro to 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    compressHTML: true,
     vite: {
         plugins: [tailwindcss()]
     }
